8ba1e71d39220d4d18353037ee78e39badf41cdc
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-04-21 16:31:08 -0500

    create: html/css skeleton

--- script.js
+++ script.js
@@ -11,3 +11,5 @@ function Book(title, author, pages, read) {
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
 
 console.log(theHobbit.info());
+
+console.log(theHobbit.valueOf());
