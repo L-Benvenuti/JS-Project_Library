90d0264289c636469836c906712fc62e9c7d4531
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-04-24 15:45:07 -0500

    create: form + JS function and EventListener to add new book to library

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
+// Creating library array and Book object construct
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
   this.info = function () {
-    return `${title} by ${author}, ${pages}, ${read}`;
+    return `${title} by ${author}, ${pages} pages, ${read}`;
   };
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
+// Adding new Book function + EventListener
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(`${title}`, `${author}`, `${pages}`, `${read}`);
+  myLibrary.push(newBook);
+  console.log('done');
+}
 
-console.log(theHobbit.info());
+const btn = document.querySelector('button');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const read = document.getElementById('read');
 
-console.log(theHobbit.valueOf());
+btn.addEventListener('click', () => { addBookToLibrary(`${title.value}`, `${author.value}`, `${pages.value}`, `${read.value}`); });
+
+// First Book
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
+myLibrary.push(theHobbit);
